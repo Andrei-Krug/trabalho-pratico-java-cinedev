1f23a12268e8b012ca4fe9fb4e0cdd36485e63d6
Andrei-Krug <andreikrug123@outlook.com>
2025-09-22 00:11:19 -0300

    Adicionei um switch para adicionar funções a cada botão, assim ligando a main com a class SalaCine.

--- trabalho-pratico-java-cinedev/src/Main.java
+++ trabalho-pratico-java-cinedev/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        
+        SalaCine sala = new SalaCine(); //Instanciando uma sala de cinema
         int option;
         
         do {
@@ -14,9 +14,45 @@ public class Main {
             System.out.println("3. Cancelar Compra de Ingresso");
             System.out.println("4. Exibir Relatório de Ocupação");
             System.out.println("5. Sair");
-            System.out.print("Escolha uma opção: ");//Menu inicial
+            System.out.print("Escolha uma opção: ");
             option = scan.nextInt();
-            
+
+            switch (option) {
+                case 1: //Chamando função para mostrar assentos
+                    sala.mostrarAssentos();
+                    break;
+
+                case 2: //Chamando fução para mostrar assento e comprar ingresso
+                    sala.mostrarAssentos();
+                    System.out.println("Digite a fila (1-10): ");
+                    int fila = scan.nextInt(); 
+                    System.out.println("Digite o assento (1-20): ");
+                    int assento = scan.nextInt();
+                    sala.compraingresso(fila, assento);
+                    break;
+
+                case 3: //Chamando função para mostrar assento e cancelar ingresso
+                    sala.mostrarAssentos();
+                    System.out.println("Digite a fila (1-10): ");
+                    fila = scan.nextInt(); 
+                    System.out.println("Digite o assento (1-20): ");
+                    assento = scan.nextInt();
+                    sala.cancelaingresso(fila, assento);
+                    break;
+
+                case 4: //Chamando funcção para mostar relatório da sala de cinema
+                    sala.relatorioDeOcupacao();
+                    break;
+
+                case 5: //Saindo do menu
+                    System.out.println("Saindo... Obrigado por usar o CineDev!");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida! Tente novamente.");
+                    break;
+            }
+
         } while (option != 5);
     }
 }
